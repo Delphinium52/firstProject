955b9b9109c553125dea1b44354c35f4bc2ffca7
{Delphinium52} <{jeasung0225@gmail.com}>
2024-04-27 21:03:18 +0900

    과제 1-2

--- src/calcProject/Main.java
+++ src/calcProject/Main.java
@@ -7,8 +7,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자 입력 : ");
+        //스캐너를 이용한 첫번째 정수 입력 받기
         int a = sc.nextInt();
         System.out.print("두 번째 숫자 입력 : ");
+        //스캐너를 이용한 두번째 정수 입력 받기
         int b = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요 : ");
+        //사칙연산 할 기호 입력 받기
+        String c = sc.next();
+        // 입력 받은값 중 첫번째 값 char 타입으로 변수 저장
+        char d = c.charAt(0);
     }
 }
