b1cb39105c1d08697527188e3b427b117b2ef90d
{Delphinium52} <{jeasung0225@gmail.com}>
2024-04-27 20:43:22 +0900

    과제 1-1

--- src/calcProject/Main.java
+++ src/calcProject/Main.java
@@ -1,4 +1,14 @@
 package calcProject;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자 입력 : ");
+        int a = sc.nextInt();
+        System.out.print("두 번째 숫자 입력 : ");
+        int b = sc.nextInt();
+    }
 }
