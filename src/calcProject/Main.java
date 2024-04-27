6f2080cc6d3a953e79a25967d632884000503282
{Delphinium52} <{jeasung0225@gmail.com}>
2024-04-27 21:39:22 +0900

    과제 1-4

--- src/calcProject/Main.java
+++ src/calcProject/Main.java
@@ -5,27 +5,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //무한 반복 탈출구
+        loopout:
+        //무한 반복
+        for(;;){
 
-        System.out.print("첫 번째 숫자 입력 : ");
-        //스캐너를 이용한 첫번째 정수 입력 받기
-        int a = sc.nextInt();
-        System.out.print("두 번째 숫자 입력 : ");
-        //스캐너를 이용한 두번째 정수 입력 받기
-        int b = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요 : ");
-        //사칙연산 할 기호 입력 받기
-        String c = sc.next();
-        // 입력 받은값 중 첫번째 값 char 타입으로 변수 저장
-        char d = c.charAt(0);
-        // 조건문을 통한 사칙연산 수행
-        int result = switch (d) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> a * b;
-            case '/' -> a / b;
-            default -> 0;
-        };
-        // 조건문을 통해 나온값 출력
-        System.out.println("결과 : " + result);
+            System.out.print("첫 번째 숫자 입력 : ");
+            //스캐너를 이용한 첫번째 정수 입력 받기
+            int a = sc.nextInt();
+            System.out.print("두 번째 숫자 입력 : ");
+            //스캐너를 이용한 두번째 정수 입력 받기
+            int b = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            //사칙연산 할 기호 입력 받기
+            String c = sc.next();
+            // 입력 받은값 중 첫번째 값 char 타입으로 변수 저장
+            char d = c.charAt(0);
+
+            // 조건문을 통한 사칙연산 수행
+            int result = switch (d) {
+                case '+' -> a + b;
+                case '-' -> a - b;
+                case '*' -> a * b;
+                case '/' -> a / b;
+                default -> 0;
+            };
+            // 조건문을 통해 나온값 출력
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산 하시겠습니까 (exit 입력시 종료)");
+            //탈출 문구 입력 받기
+            String loopbreak = sc.next();
+            //제대로 입력하면 탈출하게 하는 조건문
+            if(loopbreak.equals("exit")){
+                break loopout;
+            }
+
+
+        }
     }
 }
